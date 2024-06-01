099ceca28ae2bea6d7579a0409dcebb825182b8c
Iwumezie <chiwumezie@gmail.com>
2024-06-01 23:07:50 +0100

    add api routes

---
+++ app/api/apiClients.tsx
@@ -0,0 +1,79 @@
+import axios from "axios";
+import ApiRoutes from "./apiRoutes";
+import { LoginBuyer, RegisterBuyerRequest } from "../components/models/IRegisterBuyer";
+import { RegisterSellerRequest } from "../components/models/IRegisterSeller";
+
+
+export const API = axios.create({
+    baseURL: ApiRoutes.BASE_URL_DEV,
+});
+
+// Api call to create new user(buyer)
+export function useRegisterBuyer() {
+    /**
+     * @returns the response for the api request
+     */
+    async function registerBuyer(data: RegisterBuyerRequest) {
+
+        //Fetch message
+        const response = await API.post(ApiRoutes.RegisterBuyer, data);
+
+        //Return response
+        return response;
+    }
+    return registerBuyer;
+}
+// Api call to create new user(seller)
+export function useRegisterSeller() {
+    /**
+     * @returns the response for the api request
+     */
+    async function registerSeller(data: RegisterSellerRequest) {
+
+        //Fetch message
+        const response = await API.post(ApiRoutes.RegisterSeller, data);
+
+        //Return response
+        return response;
+    }
+    return registerSeller;
+}
+// Api call to verify user
+export function useVerifyUser() {
+    async function verifyUser(data: { id: string; code: string }) {
+        // Fire the request
+        const response = await API.post(ApiRoutes.VerifyUser, data);
+
+        // Return the response
+        return response;
+    }
+
+    return verifyUser;
+}
+
+export function useResendVerificationCode() {
+    async function resendVerificationCode(userId: string) {
+        // Fire the request
+        const response = await API.get(
+            `${ApiRoutes.ResendVerificationCode}?id=${userId}`
+        );
+
+        // Return the response
+        return response;
+    }
+
+    return resendVerificationCode;
+}
+// Api call to login buyer
+export function useLoginBuyer() {
+    async function loginBuyer(data: LoginBuyer) {
+
+        // Fire the request
+        const response = await API.post(ApiRoutes.LoginBuyer, data);
+
+        // Return the response
+        return response;
+    }
+
+    return loginBuyer;
+}
---
+++ app/api/apiRoutes.tsx
@@ -0,0 +1,27 @@
+export default class ApiRoutes {
+
+    static BASE_URL_DEV: string = 'http://localhost:3000';
+    // static BASE_URL_TEST: string = 'https://test.api.limbsimple.com.ng';
+
+    /**
+     * Api routes to register new user(buyer)
+     */
+    static RegisterBuyer: string = 'api/auth/register';
+    /**
+     * Api routes to register new user(buyer)
+     */
+    static RegisterSeller: string = 'api/auth/register/seller';
+    /**
+     * Api routes to login buyer
+     */
+    static LoginBuyer: string = 'api/auth/login';
+    /**
+     * Api routes to verify user
+     */
+    static VerifyUser: string = 'api/auth/verify';
+    /**
+     * Api routes to resend verification code
+     */
+    static ResendVerificationCode: string = 'api/auth/resend';
+
+}
