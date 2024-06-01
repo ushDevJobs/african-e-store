bc1514a74ee42f931ce539fe6e4e824387aad8d8
Iwumezie <chiwumezie@gmail.com>
2024-06-01 23:08:57 +0100

    add models

---
+++ app/components/models/IRegisterBuyer.ts
@@ -0,0 +1,18 @@
+export interface RegisterBuyerRequest {
+  email: string;
+  password: string;
+  fullname: string;
+  confirmPassword: string;
+  telephone: string;
+  country: string;
+}
+export interface RegisterBuyerResponse {
+  status: boolean;
+  message: string;
+  userId: string;
+}
+
+export interface LoginBuyer {
+  email: string;
+  password: string;
+}
---
+++ app/components/models/IRegisterSeller.ts
@@ -0,0 +1,9 @@
+export interface RegisterSellerRequest {
+  email: string;
+  password: string;
+  fullname: string;
+  confirmPassword: string;
+  telephone: string;
+  companyName: string;
+  country: string;
+}
