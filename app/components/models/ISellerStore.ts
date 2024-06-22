67c5ea01aab05045bade41734340456e18a3a1ac
Iwumezie <chiwumezie@gmail.com>
2024-06-23 00:04:57 +0100

    add seller store model

---
+++ app/components/models/ISellerStore.ts
@@ -0,0 +1,23 @@
+export interface StoreDetails {
+  name: string;
+  id: string;
+  description: string;
+  image: null;
+}
+
+export interface AvgRating {
+  rating: number;
+}
+
+export interface Rating{
+      rating: number;
+      percentage: number;
+    }
+
+export interface SellerStoreResponse {
+  storeDetails: StoreDetails;
+  avgRating: AvgRating;
+  ratingWithPercent: Rating[];
+  feedback: number;
+  totalItemSold: number;
+}
