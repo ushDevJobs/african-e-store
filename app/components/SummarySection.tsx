0c989c4e7dbe5d9c6b4fba6081085c7fbe23ad95
Iwumezie <chiwumezie@gmail.com>
2024-06-23 00:04:42 +0100

    add to cart summary section

--- app/components/SummarySection.tsx
+++ app/components/SummarySection.tsx
@@ -1,28 +1,36 @@
 import React from 'react'
 import styles from '../styles/SummarySection.module.scss'
 import Link from 'next/link'
+import { useSelector } from 'react-redux'
+import { totalPriceSelector } from '../redux/features/cart/cartSlice'
+import { RootState } from '../redux/store'
 type Props = {}
 
 const SummarySection = (props: Props) => {
-  return (
-      <div className={styles.rhs}>
-          <div className={styles.summaryItem}>
-              <p>Item (1)</p>
-              <h3>US $164.99</h3>
-          </div>
-          <div className={styles.summaryItem}>
-              <p>Shipping to Nigeria </p>
-              <h3>US $31</h3>
-          </div>
-          <div className={styles.total}>
-              <p>Subtotal </p>
-              <h3>US $195</h3>
-          </div>
-          <Link href={'/checkout'}> 
-           <button>Pay $195.99</button>
-           </Link>
-      </div>
-  )
+    const totalPrice = useSelector(totalPriceSelector);
+
+    const cartItems = useSelector((state: RootState) => state.cart.cartItems);
+    return (
+        <div className={styles.rhs}>
+            <div className={styles.summaryItem}>
+                <p>Item ({cartItems.length})</p>
+                <h3>&pound;{totalPrice.toLocaleString()}</h3>
+            </div>
+            <div className={styles.summaryItem}>
+                <p>Shipping to Nigeria </p>
+                <h3>Shipping fee here</h3>
+            </div>
+            <div className={styles.total}>
+                <p>Subtotal </p>
+                <h3>&pound;{totalPrice.toLocaleString()}+ shipping fee</h3>
+                {/* add shipping fee  */}
+            </div>
+            <Link href={'/checkout'}>
+                <button>Pay &pound;{totalPrice.toLocaleString()} + shipping fee</button>
+                {/* add shipping fee  */}
+            </Link>
+        </div>
+    )
 }
 
 export default SummarySection
