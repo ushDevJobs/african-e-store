feefbeddcd07a5d9eae66e0fbdc083c25db8f98b
Iwumezie <chiwumezie@gmail.com>
2024-06-24 21:37:18 +0100

    work on checkout page

--- app/checkout/CheckoutPage.tsx
+++ app/checkout/CheckoutPage.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import styles from './Checkout.module.scss'
 import Image from 'next/image'
@@ -5,40 +6,55 @@ import images from '@/public/images'
 import { RectangleIcon } from '../components/SVGs/SVGicons'
 import QuantityButton from '../components/QuantityButton'
 import Link from 'next/link'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../redux/store'
+import { decrement, increment, totalPriceSelector } from '../redux/features/cart/cartSlice'
 
 type Props = {}
 
 const CheckoutPage = (props: Props) => {
+    const dispatch = useDispatch()
+    const cartItems = useSelector((state: RootState) => state.cart.cartItems);
+    const totalPrice = useSelector(totalPriceSelector);
     return (
         <div className={styles.main}>
             <h1>Checkout</h1>
             <div className={styles.checkoutContainer}>
                 <div className={styles.lhs}>
                     <div className={styles.productInfos}>
-                        <h2>Item</h2>
-                        <p className={styles.seller}>
-                            Seller: Chavo global
-                        </p>
-                        <div className={styles.info}>
-                            <div className={styles.image}>
-                                <Image src={images.cart_prooduct_image} alt='product image' fill />
-                            </div>
-                            <div className={styles.item}>
-                                <p className={styles.name}>Samsung Galaxy S21 5G SM-G991U Factory Unlocked 128GB Phantom Gray C</p>
-                                <p className={styles.price}>US $164.99</p>
-                                <QuantityButton />
-                                <div className={styles.delivery}>
-                                    <h3>Delivery</h3>
-                                    <p>Delivery
-                                        Estimated delivery: Jun 19 - Jul 18Est. delivery: Jun 19 - Jul 18
-                                        Rayvvin International Shipping
-                                        US $31.00</p>
-                                </div>
+                        {/* <h2>Item</h2> */}
+                        {cartItems && cartItems.length > 0 && cartItems.map((item, index) => (
+                            <div className={styles.card} key={index}>
+                                <p className={styles.seller}>
+                                    Seller: Chavo global
+                                </p>
+                                <div className={styles.info}>
+                                    <div className={styles.image}>
+                                        <Image src={images.cart_prooduct_image} alt='product image' fill />
+                                    </div>
+                                    <div className={styles.item}>
+                                        <p className={styles.name}>{item.product.name}</p>
+                                        <p className={styles.price}>&pound;{item.product.amount.toLocaleString()}</p>
+                                        <QuantityButton
+                                            onIncrease={() => dispatch(increment(item.product))}
+                                            onDecrease={() => dispatch(decrement(item.product))}
+                                            qty={item.qty}
+                                        />
+                                        <div className={styles.delivery}>
+                                            <h3>Delivery</h3>
+                                            <p>Delivery
+                                                Estimated delivery: Jun 19 - Jul 18Est. delivery: Jun 19 - Jul 18
+                                                Rayvvin International Shipping
+                                                US $31.00</p>
+                                        </div>
 
-                                <span className={styles.fees}>Authorities may apply duties, fees, and taxes upon delivery</span>
+                                        <span className={styles.fees}>Authorities may apply duties, fees, and taxes upon delivery</span>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
+
                     <div className={styles.address}>
                         <h3>Ship to</h3>
                         <p>User address from google </p>
@@ -69,19 +85,19 @@ const CheckoutPage = (props: Props) => {
 
                     <div className={styles.payment}>
                         <div className={styles.item}>
-                            <p>Item (1)</p>
-                            <p>US $164.99</p>
+                            <p>Item  ({cartItems.length})</p>
+                            <p>&pound;{totalPrice.toLocaleString()}</p>
                         </div>
                         <div className={styles.shipping}>
                             <p>Shipping </p>
-                            <p>UUS $31</p>
+                            <p>shipping price here</p>
                         </div>
                         <div className={styles.terms}>
                             <p>By confirming your order, you agree to the Rayvinn International Shipping terms and conditions. </p>
 
-                            <p>By placing your order, you agree to eBay&apos;s User Agreement and Privacy Notice</p>
+                            {/* <p>By placing your order, you agree to eBay&apos;s User Agreement and Privacy Notice</p> */}
                             <Link href={'/payment'}>
-                                <button>Pay with  Visa $195.99</button>
+                                <button>Pay with Visa &pound;{totalPrice.toLocaleString()} + shipping</button>
                             </Link>
                         </div>
                     </div>
