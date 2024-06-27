7d3e4c73623856633e290c99fc7a0e99235d0738
Jehoshaphat Adediran <73774374+segunade-coder@users.noreply.github.com>
2024-06-27 13:12:46 +0100

    Update products.ts. working on add product

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,8 +1,14 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { prisma } from "../prisma";
 import { returnJSONSuccess } from "../utils/functions";
 
 export const updateProduct = async () => {};
+
+export const addProduct = async (req: Request, res: Response, next:NextFunction) => {
+const {name, quantity, condition,price salesType,category,date} = req.body;
+return returnJSONSuccess(res)
+}
+
 export const getProductById = async (req: Request, res: Response) => {
   const { id } = req.params;
   const product = await prisma.product.findFirst({
