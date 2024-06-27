0e1db2609a9f42fb2b8b25833d5eb9dfa8daa4d2
Jehoshaphat Adediran <73774374+segunade-coder@users.noreply.github.com>
2024-06-27 13:06:54 +0100

    Update index.ts.added route for addProduct

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { adminRoleCheck, sellerRoleCheck } from "../../middlewares/roles";
 import { rootErrorHandler } from "../../root-error-handler";
-import { getProductById, updateProduct } from "../../controllers/products";
+import { getProductById, updateProduct, addProduct } from "../../controllers/products";
+import {upload} from "../../config/configOptions"
 
 const router = Router();
 
 router
   .route("/product/:id")
   .get(rootErrorHandler(getProductById))
+   .post([sellerRoleCheck, adminRoleCheck,upload.array("images")], rootErrorHandler(addProduct))
   .patch([sellerRoleCheck, adminRoleCheck], rootErrorHandler(updateProduct));
 export { router as productRoutes };
