55b0bdf0a6e60a80290912514f54e5a9ace89048
segunade-coder <segunade041gmail.com>
2024-07-05 23:52:43 +0100

    working on payments

--- src/exceptions/validation.ts
+++ src/exceptions/validation.ts
@@ -1,4 +1,4 @@
-import { ErrorCode, HttpException } from "./root";
+import { HttpException } from "./root";
 
 export class UnprocessableEntity extends HttpException {
   constructor(message: string, errorCode: number, error: any) {
