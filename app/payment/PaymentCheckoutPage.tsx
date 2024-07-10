185b5166f6c16e4951ea5efa25e804dc43ccb1b2
segunade-coder <segunade041gmail.com>
2024-07-10 18:36:57 +0100

    integrated payment

--- app/payment/PaymentCheckoutPage.tsx
+++ app/payment/PaymentCheckoutPage.tsx
@@ -2,96 +2,107 @@
 
 import React, { useEffect, useState } from "react";
 import {
-    useStripe,
-    useElements,
-    PaymentElement,
+  useStripe,
+  useElements,
+  PaymentElement,
 } from "@stripe/react-stripe-js";
-import convertToSubcurrency from "@/lib/convertToSubcurrency";
-
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 const PaymentCheckoutPage = ({ amount }: { amount: number }) => {
-    const stripe = useStripe();
-    const elements = useElements();
-    const [errorMessage, setErrorMessage] = useState<string>();
-    const [clientSecret, setClientSecret] = useState("");
-    const [loading, setLoading] = useState(false);
+  const cartItems = useSelector((state: RootState) => state.cart.cartItems);
 
-    useEffect(() => {
-        fetch("/api/create-payment-intent", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ amount: convertToSubcurrency(amount) }),
-        })
-            .then((res) => res.json())
-            .then((data) => setClientSecret(data.clientSecret));
-    }, [amount]);
+  const stripe = useStripe();
+  const elements = useElements();
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [clientSecret, setClientSecret] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setLoading(true);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/payment/pay", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: cartItems.map((cart) => ({
+          id: cart.product.id,
+          quantity: cart.qty,
+        })),
+      }),
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.data.clientSecret));
+  }, [amount]);
 
-        if (!stripe || !elements) {
-            return;
-        }
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
 
-        const { error: submitError } = await elements.submit();
-
-        if (submitError) {
-            setErrorMessage(submitError.message);
-            setLoading(false);
-            return;
-        }
-
-        const { error } = await stripe.confirmPayment({
-            elements,
-            clientSecret,
-            confirmParams: {
-                return_url: `http://www.localhost:2500/payment-success?amount=${amount}`,
-            },
-        });
-
-        if (error) {
-            // This point is only reached if there's an immediate error when
-            // confirming the payment. Show the error to your customer (for example, payment details incomplete)
-            setErrorMessage(error.message);
-        } else {
-            // The payment UI automatically closes with a success animation.
-            // Your customer is redirected to your `return_url`.
-        }
-
-        setLoading(false);
-    };
-
-    if (!clientSecret || !stripe || !elements) {
-        return (
-            <div className="flex items-center justify-center">
-                <div
-                    className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-[#2c7865]"
-                    role="status"
-                >
-                    <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                        Loading...
-                    </span>
-                </div>
-            </div>
-        );
+    if (!stripe || !elements) {
+      return;
     }
 
+    const { error: submitError } = await elements.submit();
+
+    if (submitError) {
+      setErrorMessage(submitError.message);
+      setLoading(false);
+      return;
+    }
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      clientSecret,
+      confirmParams: {
+        return_url: `http://www.localhost:2500/payment-success?amount=${amount}`,
+      },
+    });
+
+    if (error) {
+      // This point is only reached if there's an immediate error when
+      // confirming the payment. Show the error to your customer (for example, payment details incomplete)
+      setErrorMessage(error.message);
+    } else {
+      // The payment UI automatically closes with a success animation.
+      // Your customer is redirected to your `return_url`.
+    }
+
+    setLoading(false);
+  };
+
+  if (!clientSecret || !stripe || !elements) {
     return (
-        <form onSubmit={handleSubmit} className="bg-white p-2 rounded-md outline-none">
-            {clientSecret && <PaymentElement />}
-
-            {errorMessage && <div>{errorMessage}</div>}
-
-            <button
-                disabled={!stripe || loading}
-                className="text-white w-full p-5 bg-[#2c7865] mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse"
-            >
-                {!loading ? `Pay £${amount}` : "Processing..."}
-            </button>
-        </form>
+      <div className="flex items-center justify-center">
+        <div
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-[#2c7865]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
     );
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="bg-white p-2 rounded-md outline-none"
+    >
+      {clientSecret && <PaymentElement />}
+
+      {errorMessage && <div>{errorMessage}</div>}
+
+      <button
+        disabled={!stripe || loading}
+        className="text-white w-full p-5 bg-[#2c7865] mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse"
+      >
+        {!loading ? `Pay £${amount}` : "Processing..."}
+      </button>
+    </form>
+  );
 };
 
 export default PaymentCheckoutPage;
--- app/payment/PaymentFormPage.tsx
+++ app/payment/PaymentFormPage.tsx
@@ -1,54 +1,78 @@
-import React from 'react'
-import styles from '../checkout/Checkout.module.scss'
-import { Elements } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
-import PaymentCheckoutPage from './PaymentCheckoutPage'
-import convertToSubcurrency from '@/lib/convertToSubcurrency'
+import React, { useEffect, useState } from "react";
+import styles from "../checkout/Checkout.module.scss";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import PaymentCheckoutPage from "./PaymentCheckoutPage";
+import convertToSubcurrency from "@/lib/convertToSubcurrency";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
-type Props = {}
-
-if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
-    throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
-}
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
-
+type Props = {};
+// if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+//     throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
+// }
+const stripePromise = loadStripe(
+  "pk_test_51PZM1SRuKfeerLkMfjgba7cuAOUVJ4fvhLOEyi9evCGjbSk1Y4t68wKKQJaihdeGZRqBJiaJ2BrGERdNJC2L1Yzg00glfDzD4C"
+);
 const PaymentFormPage = (props: Props) => {
-    const amount = 49.99;
-    return (
-        //   <form action="" className={styles.formContainer}>
-        //       <div className={styles.formField}>
-        //           <label htmlFor=""><span>*</span>Card number </label>
-        //           <input type="text" name="" id="" placeholder='XXX XXX XXX XXX' />
-        //       </div>
-        //       <div className={styles.formField}>
-        //           <label htmlFor=""><span>*</span>CVV </label>
-        //           <input type="text" name="" id="" placeholder='***' />
-        //       </div>
-        //       <div className={styles.formField}>
-        //           <label htmlFor=""><span>*</span>MM/YY </label>
-        //           <input type="text" name="" id="" placeholder='' />
-        //       </div>
-        //       <div className={styles.formField}>
-        //           <label htmlFor=""><span>*</span>Card pin </label>
-        //           <input type="text" name="" id="" placeholder='' />
-        //       </div>
+  const cartItems = useSelector((state: RootState) => state.cart.cartItems);
+  const [amount, setAmount] = useState(0);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/payment/amount", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: cartItems.map((cart) => ({
+          id: cart.product.id,
+          quantity: cart.qty,
+        })),
+      }),
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => setAmount(data.data.amount));
+  }, []);
+  return (
+    //   <form action="" className={styles.formContainer}>
+    //       <div className={styles.formField}>
+    //           <label htmlFor=""><span>*</span>Card number </label>
+    //           <input type="text" name="" id="" placeholder='XXX XXX XXX XXX' />
+    //       </div>
+    //       <div className={styles.formField}>
+    //           <label htmlFor=""><span>*</span>CVV </label>
+    //           <input type="text" name="" id="" placeholder='***' />
+    //       </div>
+    //       <div className={styles.formField}>
+    //           <label htmlFor=""><span>*</span>MM/YY </label>
+    //           <input type="text" name="" id="" placeholder='' />
+    //       </div>
+    //       <div className={styles.formField}>
+    //           <label htmlFor=""><span>*</span>Card pin </label>
+    //           <input type="text" name="" id="" placeholder='' />
+    //       </div>
 
-        //       <button>Continue to pay $195.99</button>
-        //   </form>
+    //       <button>Continue to pay $195.99</button>
+    //   </form>
 
-        <main className="p-5 text-white text-center rounded-md bg-[#f7fafa]">
-            <Elements
-                stripe={stripePromise}
-                options={{
-                    mode: 'payment',
-                    amount: convertToSubcurrency(amount),
-                    currency: "gbp",
-                }}
-            >
-                <PaymentCheckoutPage amount={amount} />
-            </Elements>
-        </main>
-    )
-}
+    <main className="p-5 text-white text-center rounded-md bg-[#f7fafa]">
+      {!!amount ? (
+        <Elements
+          stripe={stripePromise}
+          options={{
+            mode: "payment",
+            amount: convertToSubcurrency(amount),
+            currency: "eur",
+          }}
+        >
+          <PaymentCheckoutPage amount={amount} />
+        </Elements>
+      ) : (
+        ""
+      )}
+    </main>
+  );
+};
 
-export default PaymentFormPage
+export default PaymentFormPage;
