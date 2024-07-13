1b4b4f6b6b126c1a1fdf032daf9d4c735282b3e5
Iwumezie <chiwumezie@gmail.com>
2024-07-14 00:23:39 +0100

    modify footer style

--- app/components/Footer/footer.tsx
+++ app/components/Footer/footer.tsx
@@ -17,9 +17,10 @@ const Footer = (props: Props) => {
         <div className={styles.footerContainer}>
             {onMobile &&
                 <div className={styles.logoArea}>
-                    <div className={styles.lhs}>
+                    <div className={styles.lhs} style={{marginBottom: '20px'}}>
                         <Link href={'/'}>
-                            <Image src={images.logo} alt='logo' /></Link>
+                            <Image src={images.logo} alt='logo' />
+                            </Link>
                         {/* <h2>Your catch phrase here </h2> */}
                     </div>
                     <div className={styles.rhs}>
