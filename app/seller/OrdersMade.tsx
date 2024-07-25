f68471510f0a7a6418011710d18f70a83a2185c2
Iwumezie <chiwumezie@gmail.com>
2024-07-25 09:13:15 +0100

    chore: code  refactoring

--- app/seller/OrdersMade.tsx
+++ app/seller/OrdersMade.tsx
@@ -8,11 +8,11 @@ import { FullPageLoader } from '../Loader/ComponentLoader';
 type Props = {
     visibility: boolean;
     setVisibility: React.Dispatch<React.SetStateAction<boolean>>;
-    orders: StoreOrderResponse[] | undefined
+    selectedOrder: StoreOrderResponse | undefined
     isFetchingOrders: boolean
 }
 
-const OrdersMade = ({ visibility, setVisibility, orders, isFetchingOrders }: Props) => {
+const OrdersMade = ({ visibility, setVisibility, selectedOrder, isFetchingOrders }: Props) => {
     return (
         <ModalWrapper
             visibility={visibility}
@@ -23,20 +23,16 @@ const OrdersMade = ({ visibility, setVisibility, orders, isFetchingOrders }: Pro
                 <div className="flex flex-col gap-3 mb-5">
                     <span onClick={() => setVisibility(false)} className='cursor-pointer flex items-end justify-end ml-auto w-fit'><TimesIcon /></span>
                     <div className="flex flex-col gap-1 text-start text-[#2C7865]">
-                        {orders?.map((order, index) => (
-                            <div key={index}>
-                                <h2 className='text-2xl font-medium'>{order.products.length} {orders && order.products.length > 1 ? 'items' : 'item'} </h2>
-                                <p key={index} className='text-lg'>Order {order.orderId}</p>
+                            <div>
+                            <h2 className='text-2xl font-medium'>{selectedOrder?.products.length} {selectedOrder && selectedOrder.products.length > 1 ? 'items' : 'item'} </h2>
+                            <p className='text-lg'>Order {selectedOrder?.orderId}</p>
                             </div>
-                        ))}
-
                     </div>
                 </div>
                 <div className="flex flex-col ">
-                    {orders?.map((order, index) => (
-                        <div className="flex flex-col gap-6" key={index}>
-                            {order.products.map((product, index) => (
-                                <div className="flex gap-6">
+                        <div className="flex flex-col gap-6">
+                        {selectedOrder?.products.map((product, index) => (
+                                <div className="flex gap-6" key={index}>
                                     <div key={index} className="relative min-w-[101px] rounded-lg h-[132px]">
                                         <Image src={product.coverImage} alt='product image' fill className='object-cover rounded-lg ' />
                                     </div>
@@ -44,19 +40,17 @@ const OrdersMade = ({ visibility, setVisibility, orders, isFetchingOrders }: Pro
                                         <p className='max-w-[400px]'>{product.name}</p>
                                         <h4 className='font-medium text-xl'>&pound;{product.amount.toLocaleString()}</h4>
 
-                                        < p className='text-[#828282] text-xs' > Quantity: <span className='text-[#1E1E1E] text-base'>{order.quantity.find((q) => q.id === product.id)
+                                    < p className='text-[#828282] text-xs' > Quantity: <span className='text-[#1E1E1E] text-base'>{selectedOrder.quantity.find((q) => q.id === product.id)
                                             ?.quantity || 0}</span></p>
                                     </div>
                                 </div>
                             ))}
                         </div>
-
-                    ))}
                 </div>
-                {!orders && isFetchingOrders && (
+                {!selectedOrder && isFetchingOrders && (
                     <FullPageLoader />
                 )}
-                {!orders && !isFetchingOrders && (
+                {!selectedOrder && !isFetchingOrders && (
                     <p className='text-center text-[#333333]'>No order available</p>
                 )}
             </div>
