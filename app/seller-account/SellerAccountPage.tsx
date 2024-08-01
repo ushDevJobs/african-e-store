52b9e1d84308f62a9ed2034085280b4318ca7848
Iwumezie <chiwumezie@gmail.com>
2024-08-01 10:41:15 +0100

    add seller account page

---
+++ app/seller-account/SellerAccountPage.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { DeliveryFeeIcon, HomeIcon, LeftArrowIcon, LeftCaretArrowIcon, LogoutIcon, MessageIcon, UserIcon } from '../components/SVGs/SVGicons'
+import Image from 'next/image'
+import styles from '../stores/[storeId]/SellerStore.module.scss'
+import Link from 'next/link'
+import { accountStyle } from '../styles'
+import { SellerStoreResponse } from '../components/models/ISellerStore'
+import DeliveryFeeModal from '../components/Modal/DeliveryFeeModal'
+import BankDetailModal from '../components/Modal/BankDetailModal'
+
+type Props = {
+    isFetchingStore: boolean
+    store: SellerStoreResponse | undefined
+    // Logout: () => Promise<void>
+}
+
+const SellerAccountPage = ({ isFetchingStore, store }: Props) => {
+    const [isDeliveryFeeModalVisible, setIsDeliveryFeeModalVisible] = useState(false);
+    const [isBankDetailModalVisible, setIsBankDetailModalVisible] = useState(false);
+
+    return (
+        <>
+            <DeliveryFeeModal
+                visibility={isDeliveryFeeModalVisible}
+                setVisibility={setIsDeliveryFeeModalVisible}
+            />
+
+            <BankDetailModal
+                visibility={isBankDetailModalVisible}
+                setVisibility={setIsBankDetailModalVisible}
+            />
+
+            <div className={styles.main}>
+                <Link href={'/seller'} className="flex items-center w-full whitespace-nowrap gap-2 mb-8 mt-6">
+                    <span className='bg-[#ECF8F5] w-9 h-9 rounded-full grid place-items-center'><LeftCaretArrowIcon /></span>
+                    <h1 className='text-[#828282] text-2xl md:text-4xl font-medium'> My Account</h1>
+                </Link>
+
+                <div className="flex items-center gap-5 mb-10">
+                    {store?.storeDetails?.image ?
+                        <div className="relative h-[70px] w-[70px] md:h-[190px] md:w-[190px]">
+                            <Image
+                                src={store.storeDetails.image}
+                                alt="Logo"
+                                fill
+                                className="object-cover rounded-full"
+                            />
+                        </div> :
+                        <span className="bg-[#2C7865] h-fit p-3 rounded-full">
+                            <UserIcon />
+                        </span>
+                    }
+                    <div className="flex flex-col">
+                        <h3 className="text-lg md:text-xl lg:text-2xl text-[#828282] mb-1 font-medium">
+                            {store?.storeDetails.name}
+                        </h3>
+                        {store?.storeDetails.description &&
+                            <p className="text-sm text-[#333333] mb-1">
+                                {store?.storeDetails.description.length > 50
+                                    ? `${store.storeDetails.description.slice(0, 50)}...`
+                                    : store.storeDetails.description}
+                            </p>}
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-4">
+                    <p onClick={() => setIsBankDetailModalVisible(true)} className={`${accountStyle}`}><span><HomeIcon /></span> Bank Details</p>
+                    <p onClick={() => setIsDeliveryFeeModalVisible(true)} className={`${accountStyle}`}><span><DeliveryFeeIcon /></span> Delivery fee</p>
+                    <p className={`${accountStyle}`}><span><MessageIcon /></span> Messages</p>
+                    {/* <p onClick={() => {
+                        Logout()
+                    }}
+                     className={`${accountStyle}`}><span><LogoutIcon /></span> 
+                     Logout
+                     </p> */}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SellerAccountPage
--- app/seller-account/page.tsx
+++ app/seller-account/page.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import { useFetchSellerStore, useUpdateBankDetail } from '../api/apiClients'
+import { SellerStoreResponse } from '../components/models/ISellerStore'
+import { createCustomErrorMessages } from '../components/constants/catchError'
+import { toast } from 'sonner'
+import SellerAccountPage from './SellerAccountPage'
+import { useRouter } from 'next/navigation'
+import { BankDetailRequest } from '../components/models/IBankDetails'
 
 type Props = {}
 
 const SellerAccount = (props: Props) => {
-  return (
-    <div>SellerAccount</div>
-  )
+    const fetchSellerStore = useFetchSellerStore()
+    const router = useRouter()
+    const [store, setStore] = useState<SellerStoreResponse>()
+    const [isFetchingStore, setIsFetchingStore] = useState<boolean>(true);
+
+    async function handleFetchStore() {
+
+        // Start loader
+        setIsFetchingStore(true);
+
+        await fetchSellerStore()
+            .then((response) => {
+                // console.log("Response: ", response.data.data);
+                setStore(response.data.data);
+            })
+            .catch((error) => {
+                const errorMessage = createCustomErrorMessages(error.response?.data)
+                toast.error(errorMessage);
+            })
+            .finally(() => {
+                setIsFetchingStore(false);
+            });
+    }
+
+    useEffect(() => {
+        handleFetchStore();
+    }, []);
+    return (
+        <SellerAccountPage
+            store={store}
+            isFetchingStore={isFetchingStore}
+            // Logout={Logout}
+        />
+    )
 }
 
 export default SellerAccount
