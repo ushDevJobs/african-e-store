6e2fb40138ab5786f96a9bc1a9c47bb2e0ad418f
Iwumezie <chiwumezie@gmail.com>
2024-08-01 11:04:36 +0100

    add loading state

--- app/seller-account/SellerAccountPage.tsx
+++ app/seller-account/SellerAccountPage.tsx
@@ -7,6 +7,7 @@ import { accountStyle } from '../styles'
 import { SellerStoreResponse } from '../components/models/ISellerStore'
 import DeliveryFeeModal from '../components/Modal/DeliveryFeeModal'
 import BankDetailModal from '../components/Modal/BankDetailModal'
+import { FullPageLoader } from '../Loader/ComponentLoader'
 
 type Props = {
     isFetchingStore: boolean
@@ -35,45 +36,51 @@ const SellerAccountPage = ({ isFetchingStore, store }: Props) => {
                     <span className='bg-[#ECF8F5] w-9 h-9 rounded-full grid place-items-center'><LeftCaretArrowIcon /></span>
                     <h1 className='text-[#828282] text-2xl md:text-4xl font-medium'> My Account</h1>
                 </Link>
-
-                <div className="flex items-center gap-5 mb-10">
-                    {store?.storeDetails?.image ?
-                        <div className="relative h-[70px] w-[70px] md:h-[190px] md:w-[190px]">
-                            <Image
-                                src={store.storeDetails.image}
-                                alt="Logo"
-                                fill
-                                className="object-cover rounded-full"
-                            />
-                        </div> :
-                        <span className="bg-[#2C7865] h-fit p-3 rounded-full">
-                            <UserIcon />
-                        </span>
-                    }
-                    <div className="flex flex-col">
-                        <h3 className="text-lg md:text-xl lg:text-2xl text-[#828282] mb-1 font-medium">
-                            {store?.storeDetails.name}
-                        </h3>
-                        {store?.storeDetails.description &&
-                            <p className="text-sm text-[#333333] mb-1">
-                                {store?.storeDetails.description.length > 50
-                                    ? `${store.storeDetails.description.slice(0, 50)}...`
-                                    : store.storeDetails.description}
-                            </p>}
+                {isFetchingStore && !store ? (
+                    <div className="h-[30vh]">
+                        <FullPageLoader />
                     </div>
-                </div>
+                ) : store && !isFetchingStore ? (
+                    <div className="flex items-center gap-5 mb-10">
+                        {store.storeDetails?.image ? (
+                            <div className="relative h-[70px] w-[70px] md:h-[190px] md:w-[190px]">
+                                <Image
+                                    src={store.storeDetails.image}
+                                    alt="Logo"
+                                    fill
+                                    className="object-cover rounded-full"
+                                />
+                            </div>
+                        ) : (
+                            <span className="bg-[#2C7865] h-fit p-3 rounded-full">
+                                <UserIcon />
+                            </span>
+                        )}
+                        <div className="flex flex-col">
+                            <h3 className="text-lg md:text-xl lg:text-2xl text-[#828282] mb-1 font-medium">
+                                {store.storeDetails?.name}
+                            </h3>
+                            {store.storeDetails?.description && (
+                                <p className="text-sm text-[#333333] mb-1">
+                                    {store.storeDetails.description.length > 50
+                                        ? `${store.storeDetails.description.slice(0, 50)}...`
+                                        : store.storeDetails.description}
+                                </p>
+                            )}
+                        </div>
+                    </div>
+                ) : (
+                    <p className='text-[#828282] text-center'>No store found</p>
+                )}
 
-                <div className="flex flex-col gap-4">
-                    <p onClick={() => setIsBankDetailModalVisible(true)} className={`${accountStyle}`}><span><HomeIcon /></span> Bank Details</p>
-                    <p onClick={() => setIsDeliveryFeeModalVisible(true)} className={`${accountStyle}`}><span><DeliveryFeeIcon /></span> Delivery fee</p>
-                    <p className={`${accountStyle}`}><span><MessageIcon /></span> Messages</p>
-                    {/* <p onClick={() => {
-                        Logout()
-                    }}
-                     className={`${accountStyle}`}><span><LogoutIcon /></span> 
-                     Logout
-                     </p> */}
-                </div>
+
+                {store && (
+                    <div className="flex flex-col gap-4">
+                        <p onClick={() => setIsBankDetailModalVisible(true)} className={`${accountStyle}`}><span><HomeIcon /></span> Bank Details</p>
+                        <p onClick={() => setIsDeliveryFeeModalVisible(true)} className={`${accountStyle}`}><span><DeliveryFeeIcon /></span> Delivery fee</p>
+                        <p className={`${accountStyle}`}><span><MessageIcon /></span> Messages</p>
+                    </div>
+                )}
             </div>
         </>
     )
