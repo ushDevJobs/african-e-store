f5ad6f95eda5ca48495fca6baf6a70ba58c42f1f
Iwumezie <chiwumezie@gmail.com>
2024-08-06 23:30:13 +0100

    add local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.scss";
 import Layout from "./components/Layout";
 import NextTopLoader from 'nextjs-toploader';
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const josefinSans = Josefin_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Africa e-store website",
@@ -18,7 +20,7 @@ export default async function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body className={josefinSans.className}>
                 <NextTopLoader
                     color='#2C7865'
                     initialPosition={0.08}
