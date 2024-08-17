e1f336cb4e4b5428f6c559de338eb8b9a9a3c415
Iwumezie <chiwumezie@gmail.com>
2024-08-17 22:39:47 +0100

    add code to add review

--- app/components/Modal/ReviewModal.tsx
+++ app/components/Modal/ReviewModal.tsx
@@ -2,25 +2,32 @@ import React, { useState } from 'react'
 import ModalWrapper from './ModalWrapper'
 import { EmptyStarRatingIcon, RatingIcon, TimesIcon } from '../SVGs/SVGicons'
 import useResponsiveness from '../hooks/responsiveness-hook'
+import { PurchaseHistoryResponse, PurchaseOrderDetails } from '../models/IUserOrder'
+import { useAddReview } from '@/app/api/apiClients'
+import { toast } from 'sonner'
+import { createCustomErrorMessages } from '../constants/catchError'
 
 type Props = {
     visibility: boolean
     setVisibility: React.Dispatch<React.SetStateAction<boolean>>
+    // selectedHistory: PurchaseHistoryResponse | undefined
+    selectedHistory: PurchaseOrderDetails | undefined
+    handleFetchHistories({ clearPreviousHistories }: {
+        clearPreviousHistories?: boolean | undefined;
+    }): Promise<void>
 }
 export interface ReviewRequest {
     rating: number;
     review: string;
 }
 
-const ReviewModal = ({ visibility, setVisibility }: Props) => {
-    const windowRes = useResponsiveness();
-    const isMobile = windowRes.width && windowRes.width < 768;
-    const onMobile = typeof isMobile == 'boolean' && isMobile;
-    const onDesktop = typeof isMobile == 'boolean' && !isMobile;
+const ReviewModal = ({ visibility, setVisibility, selectedHistory, handleFetchHistories }: Props) => {
+
+    const addReview = useAddReview();
 
     const [rating, setRating] = useState<number>(0); // Initial rating is 0
     const [review, setReview] = useState<string>(''); // Initial review is empty
-
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const handleRatingClick = (value: number): void => {
         setRating(value); // Set the rating when a star is clicked
     };
@@ -29,9 +36,50 @@ const ReviewModal = ({ visibility, setVisibility }: Props) => {
         setReview(e.target.value); // Update review as the user types
     };
 
-    function handleSubmit() {
+    async function handleSubmit(e: React.FormEvent) {
+        e.preventDefault(); // Prevent form submission
 
-    }
+        // Validate the form
+        if (rating === 0 || review.trim() === '') {
+            toast.error('Please provide both a rating and a review.');
+            return;
+        }
+
+        setIsLoading(true);
+
+        const reviewData: ReviewRequest = {
+            rating,
+            review,
+        };
+        await addReview(selectedHistory?.id as string, reviewData)
+            .then((response) => {
+
+                // Log response 
+                // console.log(response);
+
+                toast.success('Review submitted successfully.');
+
+                // Fetch updated histories
+                handleFetchHistories({ clearPreviousHistories: true });
+
+                // Close the modal
+                setVisibility(false);
+
+                // Clear the rating and review
+                setRating(0);
+                setReview('');
+            })
+            .catch((error) => {
+                const errorMessage = createCustomErrorMessages(error.response?.data)
+                toast.error(errorMessage)
+
+            })
+            .finally(() => {
+
+                // Close laoder 
+                setIsLoading(false);
+            })
+    };
 
     return (
         <ModalWrapper
@@ -40,18 +88,16 @@ const ReviewModal = ({ visibility, setVisibility }: Props) => {
             styles={{ backgroundColor: 'transparent' }}>
             <div className="w-full bg-white rounded-3xl px-7 py-10 max-h-[90vh] overflow-y-auto">
                 <span onClick={() => setVisibility(false)} className="ml-auto cursor-pointer flex items-end justify-end mb-2"><TimesIcon /></span>
-                <form className="px-12">
+                <form className="px-12" onSubmit={(e) => handleSubmit(e)}>
                     <h2 className='text-center text-base md:text-xl text-[#828282] font-normal leading-7 mb-6'>Your package has been delivered <br /> please drop a rating and review </h2>
-                    {/* <div className=""> */}
+
                     <span className='flex items-center gap-6 mb-12 justify-center'>
                         {[1, 2, 3, 4, 5].map((star, index) => (
                             <span key={index} className='cursor-pointer'
                                 onClick={() => handleRatingClick(star)}
-                                style={{ cursor: 'pointer', color: star <= rating ? 'gold' : 'gray' }}
+                                style={{ cursor: 'pointer', color: star <= rating ? '#FF9800' : 'gray', fontSize: '30px' }}
                             >
-                                {/* ★ */}
-                                {onDesktop && <EmptyStarRatingIcon />}
-                                {onMobile && <RatingIcon />}
+                                ★
                             </span>
                         ))}
                     </span>
@@ -63,7 +109,7 @@ const ReviewModal = ({ visibility, setVisibility }: Props) => {
                             onChange={handleReviewChange}
                             className='bg-[#F7FAFA] resize-none py-3 outline-none h-[100px] px-5 rounded-md border border-[#ACACAC] w-full' placeholder='Enter comment here ' />
                     </div>
-                    <button className='bg-[#2C7865] w-full text-white text-sm font-medium rounded-3xl py-4 px-8' onClick={handleSubmit}>Submit Review</button>
+                    <button type='submit' className='bg-[#2C7865] w-full text-white text-sm font-medium rounded-3xl py-4 px-8 disabled:pointer-events-none disabled:opacity-50' disabled={isLoading}>Submit Review</button>
                 </form>
             </div>
         </ModalWrapper>
