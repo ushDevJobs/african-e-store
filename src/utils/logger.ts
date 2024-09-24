76e48292cec532d3014182efabca7602fcfcb401
Emmytheo <chidi.mgbara@gmail.com>
2024-09-24 21:02:29 +0100

    update

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -43,7 +43,7 @@ export const morganLogger =
           return res.statusCode < 400;
         },
         stream: fs.createWriteStream(
-          path.resolve(__dirname, "./dist/logs/access.log"),
+          path.resolve(__dirname, "logs/access.log"),
           {
             flags: "a",
           }
